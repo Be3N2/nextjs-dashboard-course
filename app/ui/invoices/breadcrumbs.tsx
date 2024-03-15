d56daecf35da487a8ea4011e6ce35e9771a67f2e
Ben Wakefield <2017WakefieldB@gmail.com>
2024-03-15 09:04:46 -0400

    postgres

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -16,6 +16,7 @@ export default function Breadcrumbs({
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
       <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
+      <ol className={`${lusitana.className} flex text-xl md:text-2xl`}>
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
@@ -23,6 +24,9 @@ export default function Breadcrumbs({
             className={clsx(
               breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
             )}
+            className={
+              breadcrumb.active ? 'text-gray-900' : 'text-gray-500'
+            }
           >
             <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
             {index < breadcrumbs.length - 1 ? (
